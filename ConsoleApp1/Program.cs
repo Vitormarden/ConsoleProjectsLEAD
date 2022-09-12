8f7d5da9b6f4bfaeead6467313110dbfb04a9efd
Vitormarden <vitomarden1234@gmail.com>
2022-09-12 15:49:43 -0300

    commit inicial

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Qual é o seu nome?");
+        var nome = Console.ReadLine();
+        Console.WriteLine("Qual sua idade?");
+        var idade = Console.ReadLine();
+        Console.WriteLine("Qual sua ocupação?");
+        var ocupação = Console.ReadLine();
+        Console.WriteLine("Qual sua remuneração bruta?");
+        string? v = Console.ReadLine();
+        var remuneração_bruta = v;
+
+        Console.WriteLine($"Olá, {nome, idade}.");
+
+    }
+}
