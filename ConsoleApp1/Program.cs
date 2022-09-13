1e816478659af5c405983d34dc65217b5c61a43c
Vitormarden <vitomarden1234@gmail.com>
2022-09-13 15:17:01 -0300

    atualização do tipo da variavel idade

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,16 +3,20 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("Qual é o seu nome?");
-        var nome = Console.ReadLine();
+        string nome = Console.ReadLine();
         Console.WriteLine("Qual sua idade?");
-        var idade = Console.ReadLine();
-        Console.WriteLine("Qual sua ocupação?");
-        var ocupação = Console.ReadLine();
-        Console.WriteLine("Qual sua remuneração bruta?");
-        string? v = Console.ReadLine();
-        var remuneração_bruta = v;
+        int idade = int.Parse(Console.ReadLine());
 
-        var saida = $"olá, {nome}. você é um {ocupação} de {idade} anos, e sua remuneração é {remuneração_bruta}";
+        Console.WriteLine("Qual sua ocupação?");
+        string ocupação = Console.ReadLine();
+        Console.WriteLine("Qual sua remuneração bruta?");
+
+        double remuneração_bruta = double.Parse(Console.ReadLine());
+       
+        double remuneração_liquida = remuneração_bruta - (remuneração_bruta * (7.5 / 100));
+
+
+        var saida = $"olá, {nome}. você é um {ocupação} de {idade} anos, e sua remuneração é {remuneração_liquida}";
 
         Console.WriteLine(saida);
 
